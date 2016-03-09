6d4557e3b3bd5b0216685ea38d375fe593ff44d9
andres0608 <alvaroandres0608@gmail.com>
2016-03-09 15:54:57 -0500

    Se crean fragment con swipe

--- app/src/main/java/com/donjacoboapp/fragments/GenovesaProductsFragment.java
+++ app/src/main/java/com/donjacoboapp/fragments/GenovesaProductsFragment.java
@@ -38,12 +38,18 @@ public class GenovesaProductsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_genovesa_products, container, false);
         recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
-//        layoutManager = new LinearLayoutManager(getActivity());
-//        recyclerView.setLayoutManager(layoutManager);
-        layoutManagerGrid = new GridLayoutManager(getActivity(),2);
-        recyclerView.setLayoutManager(layoutManagerGrid);
-        Product Genovesa = new Product("Helado Genovesa + Zepelin", 26000, R.drawable.genovesa);
-        recyclerViewAdapter = new RecyclerViewAdapter(Product.getAllProducts());
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        Product genovesa = new Product("Genovesa®", "Producto Original de Don Jacobo. Bizcochuelo bañado en leches y cubierto en chocolate rayado.",40000, R.drawable.genovesa);
+        Product milkos = new Product("Milkos","Exquisita torta esponjosa de chocolate bañada en una cremosa malteada del mismo sabor, con una sabrosa cubierta de cacao y chocolate rayado forman la combinación perfecta.",42000, R.drawable.tres_leches_chocolatoso);
+        Product genovesaNociolle = new Product("Genovesa® Nocciole*","Crema de avellanas y chocolate. ¡Única! ¡Exclusiva! ¡La nueva sensación! ¡Tienes que probarla!\n \n Esta nueva torta reúne dos grandes  creaciones gastronómicas: la original GENOVESA® de Don Jacobo Postres y Ponqués y una deliciosa crema de avellanas y chocolate. Juntas se convierten en una delicia que hacen balance en dulzura y suavidad. La torta se complementa con un novedoso cereal recubierto en chocolate blanco y chocolate semiamargo (igualmente exclusivo de Don Jacobo Postres y Ponqués), cuya crocancia le da un toque adicional que hará de ésta la preferida de chicos y grandes.",44000,R.drawable.genovesa_nociello);
+        Product cobitoGenovesa = new Product("Cobito® Genovesa", "Porción del Producto Original de Don Jacobo. Bizcochuelo bañado en leches y cubierto en chocolate rayado.", 8000, R.drawable.cobito_genovesa);
+        listProduct = new ArrayList<Product>();
+        listProduct.add(genovesa);
+        listProduct.add(milkos);
+        listProduct.add(genovesaNociolle);
+        listProduct.add(cobitoGenovesa);
+        recyclerViewAdapter = new RecyclerViewAdapter(listProduct);
         recyclerView.setAdapter(recyclerViewAdapter);
         return v;
     }
